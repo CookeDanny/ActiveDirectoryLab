a33f5d3d86046aef1ae2e8d9deed96c89e42d474
m4g1cm4n <22482285+m4g1cm4n@users.noreply.github.com>
2018-09-05 13:56:16 +0100

    Add files via upload

---
+++ install-utilities.ps1
@@ -0,0 +1,18 @@
+If (-not (Test-Path "C:\ProgramData\chocolatey")) {
+  Write-Host "Installing Chocolatey"
+  iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+  Write-Host "Chocolatey is already installed."
+}
+
+Write-Host "Installing Notepad++, Chrome, WinRar"
+If ($(hostname) -eq "win10") {
+  # Because the Windows10 start menu sucks
+  choco install -y classic-shell -installArgs ADDLOCAL=ClassicStartMenu
+}
+choco install -y NotepadPlusPlus
+choco install -y GoogleChrome
+choco install -y WinRar
+
+Write-Host "Utilties installation complete!"
+
